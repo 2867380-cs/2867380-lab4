705366b731649c1b9eb6b7aa93a64dc9450a582e
2867380-cs <2867380@students.wits.ac.za>
2026-03-02 15:39:08 +0200

    defined spinner

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
+const spinner = document.getElementById("loading-spinner");
 async function searchCountry(countryName) {
     try {
         // Show loading spinner
         spinner.classList.remove("hidden");
         // Fetch country data
         const response = await fetch(`https://restcountries.com/v3.1/name/${countryName}`);
-        const country = await response.json();
+        const data = await response.json();
+        const country = data[0];
         // Update DOM
         document.getElementById("country-info").innerHTML = 
-            <h2> ${country.name.common} </h2>
+            `<h2> ${country.name.common} </h2>
             <p><strong>Capital:</strong> ${country.capital[0]</p>
             <p><strong>Population:</strong> ${country.population.toLocaleString()}</p>
             <p><strong>Region:</strong> ${country.region}</p>
             <img src="${country.flags.svg}" alt="${country.name.common} flag">
-            ;
+            `;
         // Fetch bordering countries
         // Update bordering countries section
     } catch (error) {
